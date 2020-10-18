82fefc86cb22d489100cc0eb2cc3b1d8c2ac55aa
Clivern <hello@clivern.com>
2020-10-18 20:47:50 +0200

    update

--- examples/basic_cluster.go
+++ examples/basic_cluster.go
@@ -1,8 +1,12 @@
+// Copyright 2020 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
 	"fmt"
-    "github.com/clivern/cluster"
+	"github.com/clivern/cluster"
 )
 
 func main() {

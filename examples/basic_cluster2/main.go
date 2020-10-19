7bcfd5119481fe26b4a9cf44cb4bcf157fa5a327
Clivern <hello@clivern.com>
2020-10-19 20:44:50 +0200

    update

---
+++ examples/basic_cluster2/main.go
@@ -0,0 +1,70 @@
+// Copyright 2020 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/clivern/cluster"
+)
+
+func LaunchNode(members []string) string {
+	clus := &cluster.Cluster{}
+
+	// Generate a unique name
+	nodeName := clus.GetNodeName()
+
+	// Get a default configs
+	config := clus.GetConfig()
+	config.Name = nodeName
+	config.BindPort = 0 // assign a free port
+	config.Events = &cluster.NodeEvents{}
+
+	// Override configs
+	clus.SetConfig(config)
+
+	clus.AddLocalNode(members)
+
+	fmt.Printf(
+		"Node Name: %s Addr: %s:%d\n",
+		clus.GetLocalNode().Name,
+		clus.GetLocalNode().Addr,
+		clus.GetLocalNode().Port,
+	)
+
+	fmt.Printf(
+		"Live Nodes Count: %d\n",
+		clus.Memlist.NumMembers(),
+	)
+
+	fmt.Printf(
+		"Live Nodes List: %v\n",
+		clus.Memlist.Members(),
+	)
+
+	return fmt.Sprintf(
+		"%s:%d",
+		clus.GetLocalNode().Addr,
+		clus.GetLocalNode().Port,
+	)
+}
+
+func main() {
+	node := LaunchNode([]string{})
+
+	// Run cluster nodes
+	for i := 0; i < 5; i++ {
+		go func() {
+			LaunchNode([]string{node})
+			for {
+
+			}
+		}()
+	}
+
+	for {
+
+	}
+}
